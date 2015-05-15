685bec42f6bd4b6736ea259a9c6cd40b0cce678a
LaraiFox <LaraiFox@hotmail.com>
2015-05-15 22:34:19 +0100

    * Moved and renamed Quaternion class to the old folder and began work rebuilding it from the outline.

--- src/net/laraifox/libdev/LibDemoBoot.java
+++ src/net/laraifox/libdev/LibDemoBoot.java
@@ -1,18 +1,54 @@
 package net.laraifox.libdev;
 
-import net.laraifox.lib.display.OpenGLDisplay;
-import net.laraifox.libdev._demos.GraphicsDemo;
-import net.laraifox.libdev._old.Vector3f_old;
 import net.laraifox.libdev.math.Vector3f;
 
 import org.lwjgl.LWJGLException;
 
 public class LibDemoBoot {
 	public static void main(String[] args) throws LWJGLException {
-//		 System.out.println(new Vector3f_old(0.0000000000001f, 0.0f, 0.0f).length());
+		testFunction();
 
-		OpenGLDisplay display = new GraphicsDemo();
-		display.initialize();
-		display.start();
+		// OpenGLDisplay display = new GraphicsDemo();
+		// display.initialize();
+		// display.start();
+	}
+
+	private static void testFunction() {
+		final float increment = 0.1f;
+
+		float percentComplete = 0.0f;
+		float alpha = increment / (1000 - -1000);
+
+		float longestLengthSqN = 0.0f;
+		float longestLengthSqP = 0.0f;
+		float longestLengthN = 0.0f;
+		float longestLengthP = 0.0f;
+
+		for (float i = -1000; i < 1000; i += increment) {
+			for (float j = -1000; j < 1000; j += increment) {
+				for (float k = -1000; k < 1000; k += increment) {
+					Vector3f vector = new Vector3f(k, j, i);
+
+					if (!vector.isZero()) {
+						vector.normalize();
+
+						float lengthSq = vector.lengthSq() - 1.0f;
+						float length = vector.length() - 1.0f;
+
+						longestLengthSqN = Math.min(longestLengthSqN, lengthSq);
+						longestLengthSqP = Math.max(longestLengthSqP, lengthSq);
+						longestLengthN = Math.min(longestLengthN, length);
+						longestLengthP = Math.max(longestLengthP, length);
+					}
+				}
+			}
+
+			percentComplete += alpha;
+			System.out.println((percentComplete * 100.0f) + "% Complete");
+		}
+
+		System.out.println("");
+		System.out.println(longestLengthSqN + ", " + longestLengthSqP + "  Sq Lengths");
+		System.out.println(longestLengthN + ", " + longestLengthP + "  Lengths");
 	}
 }
